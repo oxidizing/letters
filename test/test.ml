6d63be9eea4a30e69f4bb041eb7cae02b39d7f15
Miko Nieminen <miko.nieminen@iki.fi>
2020-07-13 21:58:34 +0200

    Reindent test/test.ml after moving to ocamlformat

--- test/test.ml
+++ test/test.ml
@@ -14,40 +14,51 @@ let get_ethereal_account_details () =
   let hostname = smtp_node |> member "host" |> to_string in
   let port = smtp_node |> member "port" |> to_int in
   let with_starttls = smtp_node |> member "secure" |> to_bool |> not in
-  Lwt.return {
-    sender = username;
-    username;
-    password;
-    hostname;
-    port = Some port;
-    with_starttls;
-    ca_dir = "/etc/ssl/certs";
-  }
-
+  Lwt.return
+    {
+      sender = username;
+      username;
+      password;
+      hostname;
+      port = Some port;
+      with_starttls;
+      ca_dir = "/etc/ssl/certs";
+    }
 
 let test_send_email_using_ethereal_service config _ () =
-  let recipients = [ To "harry@example.com"; To "larry@example.com"; Cc "bill@example.com"; Bcc "dave@example.com" ] in
+  let recipients =
+    [
+      To "harry@example.com";
+      To "larry@example.com";
+      Cc "bill@example.com";
+      Bcc "dave@example.com";
+    ]
+  in
   let subject = "Hello" in
-  let body = Plain {|
+  let body =
+    Plain
+      {|
       Hi there,
 
       have you already seen the very cool new web framework written in ocaml: https://github.com/oxidizing/sihl
 
       Regards,
       The team
-|} in
+|}
+  in
   let message = build_email ~from:config.sender ~recipients ~subject ~body in
-  let* res = (send ~config ~recipients ~message) in
-  match res with
-  | Ok () -> Lwt.return ()
-  | Error msg -> failwith msg
+  let* res = send ~config ~recipients ~message in
+  match res with Ok () -> Lwt.return () | Error msg -> failwith msg
 
 (* Run it *)
 let () =
-  Lwt_main.run (
-    let* conf: config = get_ethereal_account_details () in
-    Alcotest_lwt.run "STMP client" [
-      "Send emails", [
-        Alcotest_lwt.test_case "Send one" `Quick (test_send_email_using_ethereal_service conf);
-      ];
-    ])
+  Lwt_main.run
+    (let* (conf : config) = get_ethereal_account_details () in
+     Alcotest_lwt.run "STMP client"
+       [
+         ( "Send emails",
+           [
+             Alcotest_lwt.test_case "Send one" `Quick
+               (test_send_email_using_ethereal_service conf);
+           ] );
+       ])
