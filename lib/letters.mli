0b7ebc3442c53c272c3af0ded9fdcabb3ba0b016
Miko Nieminen <miko.nieminen@iki.fi>
2020-08-04 10:52:13 +0200

    Add some API documentation

--- lib/letters.mli
+++ lib/letters.mli
@@ -7,6 +7,19 @@ type config = {
   with_starttls : bool;
   ca_dir : string;
 }
+(** Configuration providing needed information to connect the SMTP server.
+ **
+ ** [sender] email address of the user
+ ** [username] username needed for the login
+ ** [password] user's password for the login
+ ** [hostname] hostname of the SMTP server
+ ** [port] port used to connect the SMTP server or None for using default port
+ ** [with_starttls] True if start unencrypted connection and then "promote"
+ ** the connection into encrypted one. False will start TLS encrypted connection.
+ ** This library does not allow unencrypted SMTP connections.
+ ** [ca_dir] system location where all CA certificates (in PEM format) are
+ ** expected to be found when verifying server certificate.
+ ** *)
 
 type body = Plain of string | Html of string
 
@@ -18,9 +31,30 @@ val build_email :
   subject:string ->
   body:body ->
   (Mrmime.Mt.t, string) result
+(** Build an email using mrmime
+ ** This function is a helper function to simplify process of building an email
+ ** with `mrmime`. It will return result type and wrapps all exceptions into Error
+ ** [from] string representation of the email proved as a `from` field, this can
+ ** be a different email address than the [config.sender].
+ ** [recipients] list of email recipients
+ ** [subject] the single line string used as the email `subject` field
+ ** [body] string representation of the actual email message that can be either
+ ** plain text or HTML message
+ ** Returns [result] indicating if built email is valid or Error if anything
+ ** failed
+ ** *)
 
 val send :
   config:config ->
   recipients:recipient list ->
   message:Mrmime.Mt.t ->
   (unit Lwt.t)
+(** Send the previously generated email
+ ** This function expects valid configuration, list of recipients and finally a
+ ** valid `mrmime` representation of the email message.
+ ** [config] valid configuration to connect the SMTP server.
+ ** [recipients] list of valid email addresses.
+ ** [message] valid `mrmime` representation of the email message.
+ ** Runs asynchronously using Lwt and retuns unit when the message is sent
+ ** successfully. If anything fails during the process, throws an exception.
+ ** *)
