e52f732ba479d1bedd036092eed19e75c854f67f
aronerben <aron.erben@gmail.com>
2020-08-04 21:20:02 +0200

    Replace ca-certs package with local implementation
    Because we always pinning opam packages is not a sustainable solution,
    add a local implementation of the ca-certs package.

---
+++ lib/ca_certs.mli
@@ -0,0 +1,5 @@
+(* TODO delete this once https://github.com/mirage/ca-certs is on opam *)
+val detect : unit -> [> `Ca_file of Lwt_io.file_name ] option Lwt.t
+(** Detect root CAs in the operating system's trust store.
+    Returns [None] if detection did not succeed. The variants correspond to the
+    ones used in [X509_lwt] in [ocaml-tls]. *)
